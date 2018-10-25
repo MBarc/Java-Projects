49767fae4ae3d54c6e2df11a6f8462c3d8add8f5
MBarc <42979055+MBarc@users.noreply.github.com>
2018-10-25 15:17:31 -0400

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -1,15 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author: Michael Barcelo
+ * Filename: PigLatin.java
+ * Github: MBarc
  */
 package piglatin;
 
 import static java.lang.Math.random;
-/*
-Author: Michael Barcelo
-Github: MBarc
-*/
 
 import java.util.Scanner;
 import java.util.Random;
